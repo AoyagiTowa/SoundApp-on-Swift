37197be5d423b18e2d091c49b0c4e338e011b866
TowaAoyagi <tab.git.kamui@gmail.com>
2021-01-27 19:03:21 +0900

    add Sound function

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -11,10 +11,15 @@ import AVFoundation
 class ViewController: UIViewController {
     
     //変数宣言
-    @IBOutlet var drumButton: UIButton!
-    
+    @IBOutlet var drum_Button: UIButton!
+    @IBOutlet var piano_Button: UIButton!
+    @IBOutlet var guitar_Button: UIButton!
+
    //ドラムのサウンドファイルを読み込んでプレイヤーを作る処理
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name:"drumSound")!.data)
+    let pianoSoundPlayer = try!AVAudioPlayer(data:NSDataAsset(name: "pianoSound")!.data)
+    let guitarSoundPlayer = try!AVAudioPlayer(data:NSDataAsset(name: "guitarSound")!.data)
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +27,47 @@ class ViewController: UIViewController {
         
     }
     @IBAction func touchDownDrumButton(){
-        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        drum_Button.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         drumSoundPlayer.currentTime = 0
         drumSoundPlayer.play()
         
     }
     
     @IBAction func touchUpDrumButton(){
-        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+        drum_Button.setImage(UIImage(named: "drumImage"), for: .normal)
     }
+    
+    @IBAction func touchDownPianoButton(){
+        piano_Button.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        pianoSoundPlayer.currentTime = 0
+        pianoSoundPlayer.play()
+        
+    }
+    
+    @IBAction func touchUpPianoButton(){
+        piano_Button.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    
+    @IBAction func touchDownGuitarButton(){
+        guitar_Button.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+        
+    }
+    
+    @IBAction func touchUpGuitarButton(){
+        guitar_Button.setImage(UIImage(named: "guitarImage"), for: .normal)
+    }
+    
+//    func touchDownButton(image_name:String,audio_name:AVAudioPlayer,button_name:UIButton){
+//        button_name.setImage(UIImage(named: image_name), for: .normal)
+//        audio_name.currentTime = 0
+//        audio_name.play()
+//    }
+//
+//    func touchUpButton(button_name:UIButton,image_name:String){
+//        button_name.setImage(UIImage(named:image_name), for: .normal)
+//    }
 
 }
 
