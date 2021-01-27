dc2fa0e754eea8f5cb7089158a76dace75642e65
TowaAoyagi <tab.git.kamui@gmail.com>
2021-01-27 18:27:32 +0900

    sound add function

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,6 +10,9 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    //変数宣言
+    @IBOutlet var drumButton: UIButton!
+    
    //ドラムのサウンドファイルを読み込んでプレイヤーを作る処理
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name:"drumSound")!.data)
 
@@ -18,11 +21,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
         drumSoundPlayer.currentTime = 0
         drumSoundPlayer.play()
+        
     }
-
+    
+    @IBAction func touchUpDrumButton(){
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+    }
 
 }
 
